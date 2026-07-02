aa3fbf52c9734c45350561b00f2d59b7296786c2
KhaiDao <129141028+Panuwat249@users.noreply.github.com>
2026-07-02 15:39:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-let defaultData = {
-month:"มิถุนายน 2569",
-punctuality:100,
-reliability:100,
-readiness:100,
-total_distance:170475,
-total_trip:8819,
-cancel_total:0
-};
+fetch("data.json")
+.then(res => res.json())
+.then(data => {
 
-let data = JSON.parse(localStorage.getItem("dashboardData")) || defaultData;
+for(let key in data){
+let el = document.getElementById(key);
 
-document.getElementById("month").innerText = data.month;
-document.getElementById("punctuality").innerText = data.punctuality + "%";
-document.getElementById("reliability").innerText = data.reliability + "%";
-document.getElementById("readiness").innerText = data.readiness + "%";
-document.getElementById("total_distance").innerText = data.total_distance.toLocaleString();
-document.getElementById("total_trip").innerText = data.total_trip.toLocaleString();
-document.getElementById("cancel_total").innerText = data.cancel_total;
+if(el){
+if(typeof data[key] === "number"){
+if(key.includes("distance") || key.includes("trip")){
+el.innerText = data[key].toLocaleString();
+}else if(
+key.includes("ontime") ||
+key.includes("reliable") ||
+key.includes("ready")
+){
+el.innerText = data[key] + "%";
+}else{
+el.innerText = data[key];
+}
+}else{
+el.innerText = data[key];
+}
+}
+}
+
+});
