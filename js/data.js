508cbd77385cc5cbc023e2442e7fe392650608c4
KhaiDao <129141028+Panuwat249@users.noreply.github.com>
2026-07-03 14:36:44 +0700

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,124 @@
+const statistics = [
+    {
+        id: "2026-06",
+        month: "มิถุนายน 2569",
+
+        onTime: {
+            north: 100,
+            west: 100,
+            total: 100
+        },
+
+        reliability: {
+            north: 100,
+            west: 100,
+            total: 100
+        },
+
+        availability: {
+            north: 100,
+            west: 100,
+            total: 100
+        },
+
+        distance: {
+            north: 119894,
+            west: 50581,
+            total: 170475
+        },
+
+        trips: {
+            north: 5340,
+            west: 3479,
+            total: 8819
+        },
+
+        cancelled: {
+            north: 0,
+            west: 0,
+            total: 0
+        }
+    },
+
+    {
+        id: "2026-07",
+        month: "กรกฎาคม 2569",
+
+        onTime: {
+            north: 100,
+            west: 100,
+            total: 100
+        },
+
+        reliability: {
+            north: 100,
+            west: 100,
+            total: 100
+        },
+
+        availability: {
+            north: 100,
+            west: 100,
+            total: 100
+        },
+
+        distance: {
+            north: 120000,
+            west: 51000,
+            total: 171000
+        },
+
+        trips: {
+            north: 5400,
+            west: 3500,
+            total: 8900
+        },
+
+        cancelled: {
+            north: 0,
+            west: 0,
+            total: 0
+        }
+    },
+
+    {
+        id: "2026-08",
+        month: "สิงหาคม 2569",
+
+        onTime: {
+            north: 99.98,
+            west: 100,
+            total: 99.99
+        },
+
+        reliability: {
+            north: 100,
+            west: 100,
+            total: 100
+        },
+
+        availability: {
+            north: 100,
+            west: 100,
+            total: 100
+        },
+
+        distance: {
+            north: 121000,
+            west: 51500,
+            total: 172500
+        },
+
+        trips: {
+            north: 5450,
+            west: 3520,
+            total: 8970
+        },
+
+        cancelled: {
+            north: 0,
+            west: 0,
+            total: 0
+        }
+    }
+];
