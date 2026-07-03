39f49d4b6136ec1128c2b70223fc55d6b64af8a1
KhaiDao <129141028+Panuwat249@users.noreply.github.com>
2026-07-03 13:32:33 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,124 +1,126 @@
-const stati*tics = [
+const statistics = [
 
     {
-        id: "2026-*6",
-        month: "มิถุนายน 2569"*
+        id: "2026-06",
+        month: "มิถุนายน 2569",
 
         onTime: {
-            no*th: 100,
-            west: 100,
-  *         total: 100
-        },
-
-  *     reliability: {
-            no*th: 100,
-            west: 100,
-  *         total: 100
-        },
-
-  *     availability: {
-            n*rth: 100,
-            west: 100,
- *          total: 100
-        },
-
- *      distance: {
-            nort*: 119894,
-            west: 50581,*            total: 170475
-        *,
-
-        trips: {
-            no*th: 5340,
-            west: 3479,
-*           total: 8819
-        },
-*        cancelled: {
-            n*rth: 0,
-            west: 0,
-     *      total: 0
-        }
-    },
-
- *  {
-        id: "2026-07",
-       *month: "กรกฎาคม 2569",
-
-        on*ime: {
             north: 100,
-   *        west: 100,
-            tot*l: 100
+            west: 100,
+            total: 100
         },
-
-        reliabi*ity: {
-            north: 100,
-   *        west: 100,
-            tot*l: 100
-        },
-
-        availab*lity: {
-            north: 100,
-  *         west: 100,
-            to*al: 100
-        },
-
-        distan*e: {
-            north: 120000,
-  *         west: 51000,
-            *otal: 171000
-        },
-
-        t*ips: {
-            north: 5400,
-  *         west: 3500,
-            t*tal: 8900
-        },
-
-        canc*lled: {
-            north: 0,
-    *       west: 0,
-            total:*0
-        }
-    },
-
-    {
-        *d: "2026-08",
-        month: "สิงห*คม 2569",
-
-        onTime: {
-     *      north: 99.98,
-            we*t: 100,
-            total: 99.99
- *      },
 
         reliability: {
- *          north: 100,
-            *est: 100,
+            north: 100,
+            west: 100,
             total: 100
- *      },
+        },
 
         availability: {
-*           north: 100,
-           *west: 100,
+            north: 100,
+            west: 100,
             total: 100
-*       },
+        },
 
         distance: {
-   *        north: 121000,
-           *west: 51500,
-            total: 17*500
+            north: 119894,
+            west: 50581,
+            total: 170475
         },
 
         trips: {
- *          north: 5450,
-           *west: 3520,
-            total: 897*
+            north: 5340,
+            west: 3479,
+            total: 8819
         },
 
         cancelled: {
-*           north: 0,
-            w*st: 0,
+            north: 0,
+            west: 0,
             total: 0
-      * }
+        }
+    },
+
+    {
+        id: "2026-07",
+        month: "กรกฎาคม 2569",
+
+        onTime: {
+            north: 100,
+            west: 100,
+            total: 100
+        },
+
+        reliability: {
+            north: 100,
+            west: 100,
+            total: 100
+        },
+
+        availability: {
+            north: 100,
+            west: 100,
+            total: 100
+        },
+
+        distance: {
+            north: 120000,
+            west: 51000,
+            total: 171000
+        },
+
+        trips: {
+            north: 5400,
+            west: 3500,
+            total: 8900
+        },
+
+        cancelled: {
+            north: 0,
+            west: 0,
+            total: 0
+        }
+    },
+
+    {
+        id: "2026-08",
+        month: "สิงหาคม 2569",
+
+        onTime: {
+            north: 99.98,
+            west: 100,
+            total: 99.99
+        },
+
+        reliability: {
+            north: 100,
+            west: 100,
+            total: 100
+        },
+
+        availability: {
+            north: 100,
+            west: 100,
+            total: 100
+        },
+
+        distance: {
+            north: 121000,
+            west: 51500,
+            total: 172500
+        },
+
+        trips: {
+            north: 5450,
+            west: 3520,
+            total: 8970
+        },
+
+        cancelled: {
+            north: 0,
+            west: 0,
+            total: 0
+        }
     }
 
 ];
